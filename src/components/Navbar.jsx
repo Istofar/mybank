d90bbfe1b1cf5a8487a823ec1041ca945fec4118
unknown <istofar201@gmail.com>
2023-05-17 12:49:40 +0100

    bank

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useState } from "react";
+import {close, logo, menu} from "../assets"
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar"
+import Main from "./Main"
+import Business from "./Business"
+import Clients from "./Clients"
+import CTA from "./CTA"
+import Stats from "./Stats"
+import Testimonials from "./Testimonials"
+import Footer from "./Footer"
+import Billing from "./Billing"
+import CardDeal from "./CardDeal"
+
+export{
+    Navbar,
+    Billing,
+    CardDeal,
+    Business,
+    Clients,
+    CTA,
+    Stats,
+    Footer,
+    Testimonials,
+    Main,
+}
